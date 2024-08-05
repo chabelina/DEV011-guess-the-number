224711eb2b57830c75a49017335bbac57c1d368f
Elizabeth <chabelina2003@gmail.com>
2024-08-05 15:11:01 -0500

    Added:
    - GuessTheNumberGame class
    
    - 'random' and 'targetNumber' attributes
    
    - Static 'main' method

---
+++ GuessTheNumberGame.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+public class GuessTheNumberGame {
+    private Random random = new Random();
+    private int targetNumber;
+    public static void main(String[] args)
+    {
+        GuessTheNumberGame game = new GuessTheNumberGame();
+        game.targetNumber = game.random.nextInt(100)+1;
+        System.out.println("Welcome to Guess The Number!");
+    }
+}
+
