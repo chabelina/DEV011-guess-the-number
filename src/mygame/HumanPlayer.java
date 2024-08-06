51174e4ec22216c13f7b92e77e9600c09f3d8e87
Elizabeth <chabelina2003@gmail.com>
2024-08-06 12:43:27 -0500

    Refactored HumanPlayer's 'makeGuess' implementation to now handle errors when user input isn't an integer

--- src/mygame/HumanPlayer.java
+++ src/mygame/HumanPlayer.java
@@ -1,14 +1,28 @@
 package mygame;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class HumanPlayer extends Player {
     @Override
     public int makeGuess() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Player 1, enter your guess: ");
-        int guess = scanner.nextInt();
-        scanner.close();
+
+        int guess = 0;
+        boolean validInput = false;
+
+        while(!validInput)
+        {
+            System.out.println("Player 1, enter your guess: ");
+            try {
+                guess = scanner.nextInt();
+                validInput = true;
+            } catch (InputMismatchException e) {
+                System.out.print("The input value is not an integer!");
+                scanner.next();
+            }
+        }
+
         return guess;
     }
 }
